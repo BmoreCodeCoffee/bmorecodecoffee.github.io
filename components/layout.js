ee8f9f207301744b851e90b90713e2fe15a342f3
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-02-12 09:52:54 -0500

    Setting up layout

--- components/layout.js
+++ components/layout.js
@@ -1,1 +1,16 @@
 import Head from 'next/head';
+import styles from '../styles/components/layout.module.scss';
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <html lang="eng-US" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
