cd451744e87bfedce932c7a2e7651b93f387de8f
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-05-14 19:02:18 -0400

    Progress

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
 import Image from 'next/image'
+import bannerPic from '../public/bcc_banner.png'
+
+import styles from '../styles/styles.module.scss'
 
 export default function Home() {
   return (
     <Layout home>
-      This is our website.
+      <div className={styles.banner_container}>
+        <Image
+          src={bannerPic}
+          layout='fill'
+          objectFit='contain'
+          alt=""
+        />
+      </div>
     </Layout>
   )
 }
