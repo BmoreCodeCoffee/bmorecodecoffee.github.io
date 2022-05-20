485b702c80f649b6cf99b4161e9ecc0b13e7b7c7
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-05-20 16:07:05 -0400

    Revert "Add cdn"
    This reverts commit 7ec4dcd675211d53527f3db6759e3a3893aef110.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+  reactStrictMode: true,
 }
