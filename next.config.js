7ec4dcd675211d53527f3db6759e3a3893aef110
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-05-20 15:54:31 -0400

    Add cdn

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  reactStrictMode: true,
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
