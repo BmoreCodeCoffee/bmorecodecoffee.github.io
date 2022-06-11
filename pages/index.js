63006a73fc7d1134c14d13d947c5ece4b579ba1d
JP Hechter <jhechter@fearless.tech>
2022-06-11 11:53:48 -0400

    test gh pages deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import Image from 'next/image'
 import bannerPic from '../public/bcc_banner.svg'
 
 import styles from '../styles/styles.module.scss'
---
+++ public/_next/static/TrvzFm02b4yhq4GydNs5r/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/3b24afb782bba469.css","static/chunks/pages/index-ddb8cc1236c184cb.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
