7abc39b233f9b1301b98858429967b551ba09911
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2022-07-07 14:03:43 -0400

    Add link and directions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,27 @@
   <div>
     <div class="row">
       <div class="col-xs-12 center-xs">
-        <p>Join the conversation on the <a href="https://baltimoretech-slack.herokuapp.com/">Baltimore Tech Slack</a>.</p>
+        <p>Check out our next <a href="https://www.meetup.com/baltimore-code-and-coffee/events/" target="_blank">Meet Up</a>.</p>
+        <p>Join the conversation on the <a href="https://baltimoretech-slack.herokuapp.com/" target="_blank">Baltimore Tech Slack</a>.</p>
       </div>
     </div>
     <div class="row">
-      <div class="col">
-        Hello
+      <div class="col directions">
+        <h2>Directions to Spark</h2>
+        <p>Spark is located in the heart of Power Plant Live in Baltimore City's Inner Harbor.</p>
+        <p>You can park at 15 Frederick St then follow the stairs/elevator down to the first floor of the Ram's Head. Go thru the hallway until you are inside of Power Plant then make a left. We are next to the ClueRoom. There will be a number to text once you arrive for someone to let you in.</p>
       </div>
     </div>
   </div>
 </template>
 
+
 <style lang="scss">
+  @import "../assets/globals";
+  @import "../assets/theme/color_palette";
+  @import "../assets/theme/screen_sizes";
+
+  .directions {
+    padding: 0 30px 30px 30px;
+  }
 </style>
