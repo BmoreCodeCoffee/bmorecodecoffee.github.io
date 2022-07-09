e4593013e74a11f703e7fd534710e6df8b69b8b0
Casey Watts <casey.s.watts@gmail.com>
2022-07-09 19:41:26 -0400

    getting and parsing the ics feed

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import {RouterView} from 'vue-router'
 import Navigation from "./components/Navigation.vue";
 import SiteFooter from "./components/SiteFooter.vue";
+import MeetupEvents from './components/MeetupEvents.vue';
 </script>
 
 <template>
@@ -11,6 +12,7 @@ import SiteFooter from "./components/SiteFooter.vue";
     </header>
 
     <main>
+      <MeetupEvents/>
       <RouterView/>
     </main>
 
